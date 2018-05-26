90adf6c33e47162691e293481c6cb4183d0fc414
Josue Alexander Ibarra <josue@elninja.com>
2018-05-26 14:30:36 -0700

    new build

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
+  //encapsulation: ViewEncapsulation.None,
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
 
   isCollapsed: boolean;
 
+  toogleNavigation(): void {
+    this.isCollapsed = !this.isCollapsed;
+  }
+
   constructor() {
-    this.isCollapsed = false;
+    this.isCollapsed = true;
   }
 
   ngOnInit() {
